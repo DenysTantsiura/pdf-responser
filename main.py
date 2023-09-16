6036d53d6b3e2e9e110748eea032c7f47b74871f
Denys Tantsiura <ghost0002501@gmail.com>
2023-09-16 18:32:00 +0300

    added alternative chat.

--- main.py
+++ main.py
@@ -116,6 +116,34 @@ async def chat(websocket: WebSocket):
             response['message'] = "left"
             await manager.broadcast(response)
 
+# ----- alternative simplest CHAT --ok------------------------
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.connections.append(websocket)
+
+
+
+    async def broadcast(self, data: str):
+        for connection in self.connections:
+            await connection.send_text(data)
+
+
+manager = ConnectionManager()
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    while True:
+        data = await websocket.receive_text()
+        await manager.broadcast(f"Client {client_id}: {data}")
+        
 
 # ----- UPLOAD FILE -----------------------------------
 @app.post('/uploadfile/')
@@ -178,6 +206,9 @@ if __name__ == '__main__':
 
 # http://0.0.0.0:8000/docs
 # http://0.0.0.0:8000/static/index.html
+
+# http://0.0.0.0:8000/static/chat.html
+
 # http://0.0.0.0:8000/
 # http://0.0.0.0:8000/api/register
 # http://0.0.0.0:8000/chat/
